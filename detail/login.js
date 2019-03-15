2083e63aaca1c265d66783122e43799865bf9be8
tyakamyz <tyakamyz@naver.com>
2019-03-15 15:46:18 +0900

    로그인 디자인

---
+++ detail/login.js
@@ -0,0 +1,9 @@
+
+
+<div class="w-100">
+    <h2 class="mb-5">Login</h2>
+    <div class="in-line test">
+      <input class="login_common login_input" type="password" name="pwd" value="" placeholder="PASSWORD">
+      <input class="login_common login_btn" type="button" value="JOIN">
+    </div>
+</div>
