1520a861f0df8cfbf31ccf323f6b0cc4e6a9f74b
tyakamyz <tyakamyz@naver.com>
2019-03-26 12:00:13 +0900

    패스워드 암호화 적용

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var mysql = require('mysql');
 var dbconfig   = require('./config/database.js');
 var connection = mysql.createConnection(dbconfig);
 var bodyParser = require('body-parser');
+const crypto = require('crypto');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -40,16 +41,32 @@ app.post('/ajaxtest', function(req, res){
  
     var pwd = req.body.data;
     
-    connection.query('SELECT * from ty_login', function(err, rows) {
+    /*crypto.randomBytes(64, (err, buf) => {
+      crypto.pbkdf2('test', buf.toString('base64'), 102154, 64, 'sha512', (err, key) => {
+        console.log('key : ' + key.toString('base64'));
+        console.log("passhash : " + pass+key.toString('base64'));
+      });
+    });*/   //패스워드 키(salt 만들기)
+    
+   
+    
+    connection.query('SELECT pwd, salt from ty_login', function(err, rows) {
         if(err){
             throw err;
         }else{
-            console.log('POST Parameter = ' + pwd);
-            console.log('The solution is: ', rows[0].pwd);
+            //console.log('POST Parameter = ' + pwd);
+            //console.log('The solution is: ', rows[0].pwd);
+            //console.log('The solution is: ', rows[0].salt);
             
-            var db = rows[0].pwd;
+            var dbPwd = rows[0].pwd;
+            var dbSalt = rows[0].salt;
             
-            if(db==pwd){
+            pwd = crypto.createHash('sha512').update(pwd+dbSalt).digest('base64');
+            
+            //console.log("1.dbPwd : " + dbPwd);
+            //console.log("2.pwd : " + pwd);
+            
+            if(dbPwd===pwd){
                 res.send('Y');
             }else{
                 res.send('N');
