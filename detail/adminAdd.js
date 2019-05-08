84f901b0c5e867dae1157fb3e50508748924bdb7
tyakamyz <tyakamyz@naver.com>
2019-05-08 17:50:01 +0900

    파일업로드 ajax 모듈 변경

--- detail/adminAdd.js
+++ detail/adminAdd.js
@@ -3,7 +3,7 @@
         <input class="login_common admin_Lbtn" type="button" id="loginBtn" value="SAVE" onClick="javascript:jsCareerAdd();">
         <input class="login_common admin_Rbtn" type="button" id="loginBtn" value="BACK" onClick="javascript:jsLoad('adminDetail','/detail/admin.js');">
     </h2>
-    <form name="addForm" method="post" action="/careerAdd">
+    <form name="addForm" method="post" action="/careerAdd" enctype="multipart/form-data">
     <ul class="fa-ul mb-0">
         <li>
             <i class="fa-li fa fa-square"></i><input class="admin_input" type="text" name="title" value="" placeholder="TITLE">
@@ -16,6 +16,9 @@
         </li>
         <li>
             <i class="fa-li fa fa-square"></i><textarea class="admin_input" name="cont" rows="10" placeholder="CONTENT"></textarea>
+        </li>
+        <li>
+                <input type="file" name="uploadFile" />
         </li>
     </ul>
     </form>
